e4ba1ebc21e5882860fe4f86acbb7355c211777a
kystor <hcwinsjames@outlook.com>
2026-06-10 03:14:17 +0800

    Update vpngate_manager.py

--- vpngate_manager.py
+++ vpngate_manager.py
@@ -3498,10 +3498,10 @@ INDEX_HTML = r"""<!doctype html>
     .nodes-table .col-address { width: 165px; }
     .nodes-table .col-proto { width: 70px; text-align: center; }
     .nodes-table .col-location { width: 148px; }
-    .nodes-table .col-asn { width: 150px; }
-    .nodes-table .col-owner { width: 150px; }
+    .nodes-table .col-asn { width: 140px; }
+    .nodes-table .col-owner { width: 140px; }
     .nodes-table .col-quality { width: 78px; }
-    .nodes-table .col-ip-type { width: 74px; }
+    .nodes-table .col-ip-type { width: 70px; }
     .nodes-table .col-actions { width: 122px; }
 
     .nodes-table tr {
